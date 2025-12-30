c075d359b4c05d10d6d7a2ed1fee1f3412e60308
Nikhil karkra <nkarkra@salesforce.com>
2025-12-31 03:00:06 +1100

    Update ChatWidget to match exact Salesforce code structure

--- components/ChatWidget.tsx
+++ components/ChatWidget.tsx
@@ -1,34 +1,36 @@
 'use client'
 
-import { useEffect } from 'react'
 import Script from 'next/script'
 
 export default function ChatWidget() {
-  useEffect(() => {
-    // Make initEmbeddedMessaging available globally
-    if (typeof window !== 'undefined') {
-      (window as any).initEmbeddedMessaging = function() {
-        try {
-          if ((window as any).embeddedservice_bootstrap) {
-            (window as any).embeddedservice_bootstrap.settings.language = 'en_US';
-            (window as any).embeddedservice_bootstrap.init(
-              '00Dg5000002kBx3',
-              'SFTroop_Messaging_for_web',
-              'https://orgfarm-d4d03d3f06-dev-ed.develop.my.site.com/ESWSFTroopMessagingfor1767107124224',
-              {
-                scrt2URL: 'https://orgfarm-d4d03d3f06-dev-ed.develop.my.salesforce-scrt.com'
-              }
-            );
-          }
-        } catch (err) {
-          console.error('Error loading Embedded Messaging: ', err);
-        }
-      };
-    }
-  }, []);
-
   return (
     <>
+      {/* First script: Initialize the function */}
+      <Script
+        id="embedded-messaging-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            function initEmbeddedMessaging() {
+              try {
+                embeddedservice_bootstrap.settings.language = 'en_US';
+                embeddedservice_bootstrap.init(
+                  '00Dg5000002kBx3',
+                  'SFTroop_Messaging_for_web',
+                  'https://orgfarm-d4d03d3f06-dev-ed.develop.my.site.com/ESWSFTroopMessagingfor1767107124224',
+                  {
+                    scrt2URL: 'https://orgfarm-d4d03d3f06-dev-ed.develop.my.salesforce-scrt.com'
+                  }
+                );
+              } catch (err) {
+                console.error('Error loading Embedded Messaging: ', err);
+              }
+            }
+          `,
+        }}
+      />
+      
+      {/* Second script: Load bootstrap and call initEmbeddedMessaging on load */}
       <Script
         src="https://orgfarm-d4d03d3f06-dev-ed.develop.my.site.com/ESWSFTroopMessagingfor1767107124224/assets/js/bootstrap.min.js"
         strategy="afterInteractive"
